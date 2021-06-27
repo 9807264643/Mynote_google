616ccfd31a6db2e58270450e06838aaa652d1c7e
mukesh <hanmukesh590@gmail.com>
2021-06-27 21:55:06 +0530

    add homefragment

--- app/src/main/java/com/example/mynote/MainActivity.kt
+++ app/src/main/java/com/example/mynote/MainActivity.kt
@@ -4,6 +4,11 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import com.example.mynote.DashboardActivity.Dashboard
 import com.example.mynote.LoginActivity.Login
 import com.example.mynote.LoginActivity.OtpProgressbar.CircleProgressBar
@@ -11,24 +16,27 @@ import com.example.mynote.Loginwithemail.EmailLogin
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var mAuth : FirebaseAuth
+    private lateinit var mAuth: FirebaseAuth
+
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var toolbar: Toolbar
+    lateinit var frag: Fragment
+    lateinit var fragments: FragmentTransaction
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val cvp = findViewById<CircleProgressBar>(R.id.cpv)
-
         mAuth = FirebaseAuth.getInstance()
 
-
-        /** if user is not authenticated, send him to loginAcctivity to authenticate first */
-
+        toolbar = findViewById(R.id.toolbar)
+        drawerLayout = findViewById(R.id.drawer)
+        setSupportActionBar(toolbar)
+        val toogle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close )
+       drawerLayout.addDrawerListener(toogle)
+        toogle.syncState()
 
 
     }
-
-
 
 
 }
